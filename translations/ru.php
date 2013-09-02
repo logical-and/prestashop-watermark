210e3460a1c83db4970c5cf19cdd0dc4f274e8cb
And <logical_and@lavabit.com>
2013-09-02 16:11:53 +0300

    Added russian and ukrainian translations

---
+++ translations/ru.php
@@ -0,0 +1,31 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{watermark}prestashop>watermark_ee20bb60493f049175fc10c35acd2272'] = 'Водяной знак';
+$_MODULE['<{watermark}prestashop>watermark_8d7c07bcea7e80d072308e4bd4cc37b0'] = 'Защитите изображение водяным знаком.';
+$_MODULE['<{watermark}prestashop>watermark_fa214007826415a21a8456e3e09f999d'] = 'Хотите удалить?';
+$_MODULE['<{watermark}prestashop>watermark_e7bbd7019d9f73bc63f406de1422d989'] = 'для того, чтобы этот модуль работать правильно изображение Водяного знака должно быть загружено.';
+$_MODULE['<{watermark}prestashop>watermark_f26d820220f1db94ea35bb61d95a80f4'] = 'Требуется прозрачность.';
+$_MODULE['<{watermark}prestashop>watermark_78d0b1c450a31c9728488a6b0a893e8a'] = 'Недостаточный уравень прозрачности.';
+$_MODULE['<{watermark}prestashop>watermark_d31c2d99614d8e16430d2d8c99c1f2b0'] = 'Необходимо выравнивание по оси - Y.';
+$_MODULE['<{watermark}prestashop>watermark_8fe9c39f4bf87082caabcf3650970c71'] = 'Выравнивание по оси Y вне разрешенного диапазона.';
+$_MODULE['<{watermark}prestashop>watermark_c2cf8e33c907a4cc689881dc8fa571c2'] = 'Необходимо выравнивание по оси - X.';
+$_MODULE['<{watermark}prestashop>watermark_3a1f788dbe8957be92048606cf0d3fcb'] = 'Выравнивание по оси X вне разрешенного диапазона.';
+$_MODULE['<{watermark}prestashop>watermark_a9cac4be0fa0b815376b96f49e1435d7'] = 'Необходимо выбрать хотя бы один тип изображений.';
+$_MODULE['<{watermark}prestashop>watermark_b670770ad59c1f8e7fb65f276074172b'] = 'Изображение должно быть в формате GIF.';
+$_MODULE['<{watermark}prestashop>watermark_130aab6764f25267c79cef371270eb2a'] = 'Ошибка при загрузке водяного знака (watermark): %1$s к %2$s';
+$_MODULE['<{watermark}prestashop>watermark_444bcb3a3fcf8389296c49467f27e1d6'] = 'ok';
+$_MODULE['<{watermark}prestashop>watermark_c888438d14855d7d96a2724ee9c306bd'] = 'Настройки обновлены';
+$_MODULE['<{watermark}prestashop>watermark_c0275788cf97660b52ab23f1f8c7c8d7'] = 'Детали водяного знака';
+$_MODULE['<{watermark}prestashop>watermark_cc99ba657a4a5ecf9d2d7cb974d25596'] = 'Как только Вы настроили модуль, восстановите изображения, используя инструмент \"Картинки\" в \"Настройках\". Однако, отметка Водяного знака будет добавлена автоматически к новым изображениям.';
+$_MODULE['<{watermark}prestashop>watermark_c23549e174914e9796db47d27c49eb95'] = 'Не загружены водяные знаки.';
+$_MODULE['<{watermark}prestashop>watermark_c9519bff5a6fe31e0f26b1c52dad7ad8'] = 'Файл водяного знака';
+$_MODULE['<{watermark}prestashop>watermark_ce4ca541df51a63fd8e78e0da29e1d44'] = 'Должно быть в формате GIF';
+$_MODULE['<{watermark}prestashop>watermark_3ccc25c1409aec574beb82bad14b5ebc'] = 'Прозрачность водяного знака (0-100)';
+$_MODULE['<{watermark}prestashop>watermark_acf0fa79a73731e7d70cb208a62c249a'] = 'Выравнивание водяного знака по X';
+$_MODULE['<{watermark}prestashop>watermark_489f2e730102b59aec48b5c27d2cbe1c'] = 'Выравнивание водяного знака по Y';
+$_MODULE['<{watermark}prestashop>watermark_8d92a725c5abead5353e60e0b2fc7d6d'] = 'Выберите типы изображений для защиты водяным знаком .';
+$_MODULE['<{watermark}prestashop>watermark_b17f3f4dcf653a5776792498a9b44d6a'] = 'Обновить настройки';
+$_MODULE['<{watermark}prestashop>watermark_220d9102465c9223781b8f99af48c154'] = 'Изображение водяного знака не является реальным GIF, пожалуйста, преобразуйте изображение.';
---
+++ translations/uk.php
@@ -0,0 +1,30 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{watermark}prestashop>watermark_ee20bb60493f049175fc10c35acd2272'] = 'Водяний знак';
+$_MODULE['<{watermark}prestashop>watermark_8d7c07bcea7e80d072308e4bd4cc37b0'] = 'Захист малюнків водяним знаком';
+$_MODULE['<{watermark}prestashop>watermark_fa214007826415a21a8456e3e09f999d'] = 'Ви впевнені, що хочете видалити цю інформацію?';
+$_MODULE['<{watermark}prestashop>watermark_e7bbd7019d9f73bc63f406de1422d989'] = 'Зображення водяного знаку повинно бути завантажено для того, щоб цей модуль працював правильно.';
+$_MODULE['<{watermark}prestashop>watermark_f26d820220f1db94ea35bb61d95a80f4'] = 'Вкажіть прозорість.';
+$_MODULE['<{watermark}prestashop>watermark_78d0b1c450a31c9728488a6b0a893e8a'] = 'Значення прозорості поза межами діапазону.';
+$_MODULE['<{watermark}prestashop>watermark_d31c2d99614d8e16430d2d8c99c1f2b0'] = 'Необхідно вказати Y-Align.';
+$_MODULE['<{watermark}prestashop>watermark_8fe9c39f4bf87082caabcf3650970c71'] = 'Значення Y-Align поза межами діапазону.';
+$_MODULE['<{watermark}prestashop>watermark_c2cf8e33c907a4cc689881dc8fa571c2'] = 'Необхідно вказати X-Align.';
+$_MODULE['<{watermark}prestashop>watermark_3a1f788dbe8957be92048606cf0d3fcb'] = 'Значення X-Align поза межами діапазону.';
+$_MODULE['<{watermark}prestashop>watermark_a9cac4be0fa0b815376b96f49e1435d7'] = 'Необхідно вказати хоча б один тип малюнків.';
+$_MODULE['<{watermark}prestashop>watermark_b670770ad59c1f8e7fb65f276074172b'] = 'Зображення повинно бути в форматі GIF.';
+$_MODULE['<{watermark}prestashop>watermark_444bcb3a3fcf8389296c49467f27e1d6'] = 'ОК';
+$_MODULE['<{watermark}prestashop>watermark_c888438d14855d7d96a2724ee9c306bd'] = 'Налаштування оновлено';
+$_MODULE['<{watermark}prestashop>watermark_c0275788cf97660b52ab23f1f8c7c8d7'] = 'Деталі водяного знаку';
+$_MODULE['<{watermark}prestashop>watermark_cc99ba657a4a5ecf9d2d7cb974d25596'] = 'Після того як ви встановили модуль, регенеруйте зображення, використовуючи \"Зображення\" в пункті меню \"Налаштування\". Також водяні знаки будуть автоматично додані на нові зображення.';
+$_MODULE['<{watermark}prestashop>watermark_c23549e174914e9796db47d27c49eb95'] = 'немає завантажених водяних знаків';
+$_MODULE['<{watermark}prestashop>watermark_c9519bff5a6fe31e0f26b1c52dad7ad8'] = 'Файл водяного знаку';
+$_MODULE['<{watermark}prestashop>watermark_ce4ca541df51a63fd8e78e0da29e1d44'] = 'Повинен бути в форматі GIF';
+$_MODULE['<{watermark}prestashop>watermark_3ccc25c1409aec574beb82bad14b5ebc'] = 'Прозорість водяного знаку (0-100)';
+$_MODULE['<{watermark}prestashop>watermark_acf0fa79a73731e7d70cb208a62c249a'] = 'X-Align водяного знаку';
+$_MODULE['<{watermark}prestashop>watermark_489f2e730102b59aec48b5c27d2cbe1c'] = 'Y-Align водяного знаку';
+$_MODULE['<{watermark}prestashop>watermark_8d92a725c5abead5353e60e0b2fc7d6d'] = 'Виберіть типи малюнків для захисту водяним знаком';
+$_MODULE['<{watermark}prestashop>watermark_b17f3f4dcf653a5776792498a9b44d6a'] = 'Оновити налаштування';
+$_MODULE['<{watermark}prestashop>watermark_220d9102465c9223781b8f99af48c154'] = 'Зображення водяного знаку не є коректним gif - файлом, будь ласка, конвертуйте його в gif.';
